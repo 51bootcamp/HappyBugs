fc043bbe420e3b06ba80dc017420904ca154d52f
heeji <heeji@MacBook-Pro-3.local>
2019-01-17 15:13:36 -0800

    iss#21, #22: signin

--- Backend/router/v1/user.js
+++ Backend/router/v1/user.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../../models');
-const crypto = require('crypto')
+const crypto = require('crypto');
+const session = require('express-session');
+
+router.use(session({
+  key: 'sid',
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000*60*15
+  }
+}));
 
 router.use((req, res, next) => {
   console.log("user router");
@@ -12,16 +23,34 @@ router.all('/', (req, res) => {
   res.send("this is user root");
 });
 
-router.post('/signup', (req, res) => {
-  let hashPassword = crypto.createHash("sha512").update(req.body.password).digest("hex");
+router.get('/signin', (req, res) => {
+  let session = req.session;
 
-  models.user.create({
-    email: req.body.email,
-    password: hashPassword,
+  models.user.findAll({
+    where: {
+      email: req.query.email
+    }
   }).then(result => {
-    console.log("everything is good");
-    res.send(result);
+    if (result == "") {
+      res.json({msg: "User does not exist"});
+    } else {
+      let dbPassword = result[0].dataValues.password;
+      let hashPassword = crypto.createHash("sha512").update(req.query.password).digest("hex");
+      if(dbPassword == hashPassword) {
+        req.session.email = req.query.email;
+        res.json({msg: req.session.email});
+      } else {
+        res.json({msg: "Password not Match"});
+      };
+    };
+  }).catch( err => {
+    res.json({msg: "err"});
   });
+});
+
+router.get('/signout', (req, res) => {
+  req.session.destroy();
+  res.clearCookie('sid');
 });
 
 module.exports = router;
