cb2f2db706be1860734a8d41258f4c70cf069de8
eunyyyoung <any218@hanmail.net>
2019-01-16 18:38:17 -0800

    modified according to the minho's comment

--- Backend/router/v1/report.js
+++ Backend/router/v1/report.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('./report.ctrl')
 
-router.use((req,res,next) => {
+router.use((req, res, next) => {
   console.log("report router");
   next();
 });
 
-router.all('/', (req,res) => {
+router.all('/', (req, res) => {
   res.send("this is report root");
 });
 
