21a6a5b2f91cf56376a9fd12c95c8609087e936b
eunyyyoung <any218@hanmail.net>
2019-01-20 14:53:08 -0800

    Issue #42: make report API for edit part

--- Backend/router/v1/report.ctrl.js
+++ Backend/router/v1/report.ctrl.js
@@ -10,32 +10,42 @@ const createReport = (req, res) => {
   }).then((result) => {
     res.status(201);
     res.send("good");
+  }).catch((err) => {
+    res.json({msg: "error"});
   });
 }
 
 const showReportList = (req, res) => {
   models.report.findAll().then((result) => {
     res.json(result);
-  }).catch((err) => {});
+  }).catch((err) => {
+    res.json({msg: "error"});
+  });
 }
 
 const findReport = (req, res) => {
-  const reportId = parseInt(req.params.reportId)
+  const reportId = parseInt(req.query.reportId);
   //When ID is not a number
   if (Number.isNaN(reportId)) {
     return res.status(400).end()
-  }
+  };
   models.report.findAll({
     where: {
       id: reportId
     }
-  }).then((result) => {
-    res.json(result);
-  }).catch((err) => {});
+  }).then ((result) => {
+    if(result == ""){
+      res.json({msg: "ReportId not exist"});
+    } else {
+      res.json(result);
+    };
+  }).catch((err) => {
+    res.json({msg: "error"});
+  });
 }
 
 const deleteReport = (req, res) => {
-  const reportId = parseInt(req.params.reportId)
+  const reportId = parseInt(req.query.reportId);
   //When ID is not a number
   if (Number.isNaN(reportId)) {
     return res.status(400).end()
@@ -45,12 +55,22 @@ const deleteReport = (req, res) => {
       id: reportId
     }
   }).then((result) => {
-    res.status(204).json(result);  //no content
-  }).catch((err) => {});
+    if(result == ""){
+      res.json({msg: "ReportId not exist"});
+    } else {
+      res.status(204).json(result);  //no content
+    };
+  }).catch((err) => {
+    res.json({msg: "error"});
+  });
 }
 
 const editReport = (req, res) => {
-  const reportId = parseInt(req.params.reportId);
+  const reportId = parseInt(req.query.reportId);
+  //When ID is not a number
+  if (Number.isNaN(reportId)) {
+    return res.status(400).end()
+  }
   const newWhat = req.body.data[0].what;
   const newLocation = req.body.data[0].location;
   const newTime = req.body.data[0].time;
@@ -66,8 +86,14 @@ const editReport = (req, res) => {
     },{
       where: {id: reportId}
   }).then((result) => {
-    res.send("good");
-  }).catch((err) => {});
+    if(result == ""){
+      res.json({msg: "ReportId not exist"});
+    } else {
+      res.send("good");
+    };
+  }).catch((err) => {
+    res.json({msg: "error"});
+  });
 }
 
 module.exports = {
