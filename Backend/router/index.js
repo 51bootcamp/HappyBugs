32a5f26646e0847f1eb20f2b7adffd84ea827658
LChoiSH <csh42504@google.com>
2019-01-24 16:21:52 -0800

    Issue #45 #47 #48 : passport

--- Backend/router/index.js
+++ Backend/router/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const version = 'v1';
-const userrouter = require('./' + version + '/user');
-const reportrouter = require('./' + version + '/report');
+module.exports = (passport) => {
+  const version = 'v1';
+  const userrouter = require('./' + version + '/user')(passport);
+  const reportrouter = require('./' + version + '/report')();
 
-router.use((req, res, next) => {
-  console.log("index page");
-  console.log(req.path);
-  next();
-});
+  router.use((req, res, next) => {
+    next();
+  });
 
-router.use('/' + version + '/user', userrouter);
-router.use('/' + version + '/report', reportrouter);
+  router.use('/' + version + '/user', userrouter);
+  router.use('/' + version + '/report', reportrouter);
 
-module.exports = router;
+  return router;
+};
--- Backend/router/v1/report.ctrl.js
+++ Backend/router/v1/report.ctrl.js
@@ -1,51 +1,69 @@
 const models = require('../../models');
 
 const createReport = (req, res) => {
-  models.report.create({
-    what: req.body.data[0].what,
-    location: req.body.data[0].location,
-    time: req.body.data[0].time,
-    who: req.body.data[0].who,
-    details: req.body.data[0].details,
-  }).then((result) => {
-    res.status(201);
-    res.send("good");
-  }).catch((err) => {
-    res.json({msg: "error"});
-  });
-}
+  if(req.isAuthenticated()){
+    models.report.create({
+      what: req.body.data[0].what,
+      location: req.body.data[0].location,
+      time: req.body.data[0].time,
+      who: req.body.data[0].who,
+      details: req.body.data[0].details,
+      userID: req.user[0].dataValues.id
+    }).then((result) => {
+      res.status(201);
+      res.json({
+        msg: "Report is made successfully"
+      });
+    });
+  } else {
+    res.status(403);
+    res.json({
+      msg : "You are not logged in"
+    });
+  }
+};
 
 const showReportList = (req, res) => {
-  models.report.findAll().then((result) => {
-    res.json(result);
-  }).catch((err) => {
-    res.json({msg: "error"});
-  });
-}
+  if(req.isAuthenticated()){
+    models.report.findAll({
+      where: {
+        userID: req.user[0].dataValues.id
+      }
+    }).then((result) => {
+      res.status(200);
+      res.json(result);
+    }).catch((err) => {});
+  } else{
+    res.status(401)
+    res.json({
+      msg: "get report failed"
+    })
+  }
+};
 
 const findReport = (req, res) => {
-  const reportId = parseInt(req.query.reportId);
-  //When ID is not a number
-  if (Number.isNaN(reportId)) {
-    return res.status(400).end()
-  };
-  models.report.findAll({
-    where: {
-      id: reportId
+  if (req.isAuthenticated()) {
+    const reportId = parseInt(req.query.reportId);
+
+    if (Number.isNaN(reportId)) {
+      return res.status(400).end()
     }
-  }).then ((result) => {
-    if(result == ""){
-      res.json({msg: "ReportId not exist"});
-    } else {
+    models.report.findAll({
+      where: {
+        id: reportId
+      }
+    }).then((result) => {
       res.json(result);
-    };
-  }).catch((err) => {
-    res.json({msg: "error"});
-  });
-}
+    }).catch((err) => {});
+  } else {
+    res.json({
+      msg: "Can't find this report"
+    });
+  }
+};
 
 const deleteReport = (req, res) => {
-  const reportId = parseInt(req.query.reportId);
+  const reportId = parseInt(req.params.reportId);
   //When ID is not a number
   if (Number.isNaN(reportId)) {
     return res.status(400).end()
@@ -55,15 +73,49 @@ const deleteReport = (req, res) => {
       id: reportId
     }
   }).then((result) => {
-    if(result == ""){
-      res.json({msg: "ReportId not exist"});
-    } else {
-      res.status(204).json(result);  //no content
-    };
-  }).catch((err) => {
-    res.json({msg: "error"});
-  });
-}
+    res.status(204).json(result);  //no content
+  }).catch((err) => {});
+};
+
+const editReport = (req, res) => {
+  const reportId = parseInt(req.query.reportId);
+  //When ID is not a number
+  if (Number.isNaN(reportId)) {
+    return res.status(400).end()
+  }
+
+  const newWhat = req.body.data[0].what;
+  const newLocation = req.body.data[0].location;
+  const newTime = req.body.data[0].time;
+  const newWho = req.body.data[0].who;
+  const newDetails = req.body.data[0].details;
+
+  if(req.isAuthenticated()){
+    models.report.update({
+        what: newWhat,
+        location: newLocation,
+        time: newTime,
+        who: newWho,
+        details: newDetails
+      },{
+        where: {id: reportId}
+    }).then((result) => {
+      res.status(200);
+      res.json({
+        msg: "update success"
+      });
+    }).catch((err) => {
+      res.json({
+        msg: "update fail"
+      });
+    });
+  } else {
+    res.status(401);
+    res.json({
+      msg: "you are not logged in"
+    });
+  }
+};
 
 const editReport = (req, res) => {
   const reportId = parseInt(req.query.reportId);
@@ -78,11 +130,11 @@ const editReport = (req, res) => {
   const newDetails = req.body.data[0].details;
 
   models.report.update({
-      what: newWhat, 
+      what: newWhat,
       location: newLocation,
       time: newTime,
       who: newWho,
-      details: newDetails 
+      details: newDetails
     },{
       where: {id: reportId}
   }).then((result) => {
@@ -102,4 +154,4 @@ module.exports = {
   findReport,
   deleteReport,
   editReport
-}
+};
