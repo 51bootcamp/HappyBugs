c8a241c53736a7de4a3a7fe0638d19b285788daf
KimKyoungjin <Jinny@KimKyoungjinui-MacBook.local>
2019-02-02 15:20:42 -0800

    Issue#123-add-dialog-onCloseReport

---
+++ Frontend/app/src/main/java/io/happybugs/happybugs/activity/CloseReportDialog.java
@@ -0,0 +1,38 @@
+package io.happybugs.happybugs.activity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+
+import io.happybugs.happybugs.R;
+
+public class CloseReportDialog extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_msg)
+                .setTitle(R.string.dialog_title)
+                .setPositiveButton(R.string.dialog_yes_btn, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(getContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton(R.string.dialog_no_btn, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
